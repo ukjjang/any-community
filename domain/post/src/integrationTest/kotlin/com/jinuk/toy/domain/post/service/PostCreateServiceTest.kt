89e0261461fa3b5628db3961207df2b6f0c2cdf7
jinukix <jinukix@musinsa.com>
2024-10-09 17:44:20 +0900

    PostCreateService IntegrationTest 작성

---
+++ domain/post/src/integrationTest/kotlin/com/jinuk/toy/domain/post/service/PostCreateServiceTest.kt
@@ -0,0 +1,30 @@
+package com.jinuk.toy.domain.post.service
+
+import com.jinuk.toy.domain.IntegrationTest
+import com.jinuk.toy.domain.post.repository.PostRepository
+import com.jinuk.toy.domain.post.service.command.PostCreateCommand
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+internal class PostCreateServiceTest(
+    private val postCreateService: PostCreateService,
+    private val postRepository: PostRepository,
+) : IntegrationTest, DescribeSpec(
+    {
+        describe("post create") {
+            context("get PostCreateCommand") {
+
+                val title = "title"
+                val command = PostCreateCommand(title)
+
+                it("create success") {
+                    val post = postCreateService(command)
+                    val postEntity = postRepository.findById(post.id!!)
+
+                    post shouldBe postEntity
+                    post.title shouldBe title
+                }
+            }
+        }
+    },
+)
