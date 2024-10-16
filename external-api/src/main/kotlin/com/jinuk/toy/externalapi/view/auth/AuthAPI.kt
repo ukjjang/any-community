93af25244f06881db66b50252ab349f17f8d76c8
jinukix <wfo3977@gmail.com>
2024-10-16 18:37:31 +0900

    API 패스 수정

--- external-api/src/main/kotlin/com/jinuk/toy/externalapi/view/auth/AuthAPI.kt
+++ external-api/src/main/kotlin/com/jinuk/toy/externalapi/view/auth/AuthAPI.kt
@@ -1,27 +1,25 @@
 package com.jinuk.toy.externalapi.view.auth
 
 import com.jinuk.toy.domain.user.service.UserAuthService
+import com.jinuk.toy.externalapi.global.ExternalAPIController
 import com.jinuk.toy.externalapi.view.auth.request.AuthCredentialsRequest
 import com.jinuk.toy.externalapi.view.auth.request.toUserCredentials
 import com.jinuk.toy.externalapi.view.auth.response.AuthLoginResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "인증")
-@RequestMapping("/auth")
-@RestController
+@ExternalAPIController
 class AuthAPI(
     private val authService: UserAuthService,
 ) {
 
-    @PostMapping("/login")
+    @PostMapping("/v1/auth/login")
     fun login(@RequestBody authCredentialsRequest: AuthCredentialsRequest) =
         AuthLoginResponse(authService.login(authCredentialsRequest.toUserCredentials()))
 
-    @PostMapping("/signup")
+    @PostMapping("/v1/auth/signup")
     fun signup(@RequestBody authCredentialsRequest: AuthCredentialsRequest) {
         authService.signUp(authCredentialsRequest.toUserCredentials())
     }
--- external-api/src/main/kotlin/com/jinuk/toy/externalapi/view/user/UserAPI.kt
+++ external-api/src/main/kotlin/com/jinuk/toy/externalapi/view/user/UserAPI.kt
@@ -1,9 +1,10 @@
-package com.jinuk.toy.externalapi.view.follow
+package com.jinuk.toy.externalapi.view.user
 
 import com.jinuk.toy.applicaiton.follow.command.FollowCommandBus
 import com.jinuk.toy.applicaiton.follow.command.usecase.CreateFollowCommand
 import com.jinuk.toy.applicaiton.follow.command.usecase.UnFollowCommand
 import com.jinuk.toy.domain.user.FollowRelation
+import com.jinuk.toy.externalapi.global.ExternalAPIController
 import com.jinuk.toy.externalapi.global.security.AuthRole
 import com.jinuk.toy.externalapi.global.security.AuthUser
 import io.swagger.v3.oas.annotations.Operation
@@ -13,19 +14,16 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@Tag(name = "팔로우")
-@RequestMapping("/follow")
-@RestController
-class FollowAPI(
+@Tag(name = "유저")
+@ExternalAPIController
+class UserAPI(
     private val followCommandBus: FollowCommandBus
 ) {
 
     @Operation(summary = "팔로우")
     @Secured(AuthRole.USER)
-    @PostMapping("/{followingUserId}")
+    @PostMapping("/v1/user/{followingUserId}/follow")
     fun follow(
         @AuthenticationPrincipal user: AuthUser,
         @PathVariable followingUserId: Long,
@@ -35,7 +33,7 @@ class FollowAPI(
 
     @Operation(summary = "언팔로우")
     @Secured(AuthRole.USER)
-    @DeleteMapping("/{followingUserId}")
+    @DeleteMapping("/v1/user/{followingUserId}/follow")
     fun unFollow(
         @AuthenticationPrincipal user: AuthUser,
         @PathVariable followingUserId: Long,
