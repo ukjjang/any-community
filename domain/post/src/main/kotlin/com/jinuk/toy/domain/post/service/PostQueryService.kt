497a690d818592b0c182a5fc678e9ef3d82527c8
jinukix <jinukix@musinsa.com>
2024-10-19 12:56:13 +0900

    게시글 단일 조회 기능에 캐싱 설정

--- domain/post/src/main/kotlin/com/jinuk/toy/domain/post/service/PostQueryService.kt
+++ domain/post/src/main/kotlin/com/jinuk/toy/domain/post/service/PostQueryService.kt
@@ -1,14 +1,24 @@
 package com.jinuk.toy.domain.post.service
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.jinuk.toy.domain.post.Post
 import com.jinuk.toy.domain.post.jpa.PostRepository
+import com.jinuk.toy.infra.redis.cache.cached
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import java.time.Duration
 
 @Service
 class PostQueryService(
     private val postRepository: PostRepository,
 ) {
-    fun getById(id: Long) = postRepository.findById(id) ?: throw NoSuchElementException("존재하지 않는 게시글입니다.")
+    fun getById(id: Long) = cached(
+        key = cacheKeyByGetById(id),
+        expire = Duration.ofMinutes(10),
+        returnType = object : TypeReference<Post>() {},
+    ) {
+        postRepository.findById(id) ?: throw NoSuchElementException("존재하지 않는 게시글입니다.")
+    }
 
     fun existsByTitle(title: String) = postRepository.existsByTitle(title)
 
@@ -24,3 +34,5 @@ class PostQueryService(
 
     fun findByOrderByIdDesc(pageable: Pageable) = postRepository.findByOrderByIdDesc(pageable)
 }
+
+fun cacheKeyByGetById(id: Long) = "PostQueryService.getById.id:$id"
