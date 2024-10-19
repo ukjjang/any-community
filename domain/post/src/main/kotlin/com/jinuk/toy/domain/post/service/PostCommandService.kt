851fbab665e2bb3e0bc6712635c3e0e6387fe279
jinukix <jinukix@musinsa.com>
2024-10-19 12:56:36 +0900

    게시글 삭제 및 수정시 단일 게시글 조회 캐시 evict

--- domain/post/src/main/kotlin/com/jinuk/toy/domain/post/service/PostCommandService.kt
+++ domain/post/src/main/kotlin/com/jinuk/toy/domain/post/service/PostCommandService.kt
@@ -2,6 +2,7 @@ package com.jinuk.toy.domain.post.service
 
 import com.jinuk.toy.domain.post.Post
 import com.jinuk.toy.domain.post.jpa.PostRepository
+import com.jinuk.toy.infra.redis.cache.cacheEvict
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,14 +10,14 @@ class PostCommandService(
     private val postQueryService: PostQueryService,
     private val postRepository: PostRepository,
 ) {
-    fun save(post: Post): Post {
-        return postRepository.save(post)
-    }
+    fun save(post: Post) = postRepository.save(post)
 
     fun delete(
         postId: Long,
         deleteUserId: Long,
     ) = postQueryService.getById(postId).apply {
         require(userId == deleteUserId) { "작성자만 게시글을 삭제할 수 있습니다." }
-    }.let(postRepository::delete)
+    }.let(postRepository::delete).also {
+        cacheEvict(cacheKeyByGetById(postId))
+    }
 }
