2970522b83a517f75bef62d70568e499209e41ab
jinukix <jinukix@musinsa.com>
2024-10-20 12:35:21 +0900

    add LikeTarget validate

--- domain/like/src/main/kotlin/com/jinuk/toy/domain/like/LikeTarget.kt
+++ domain/like/src/main/kotlin/com/jinuk/toy/domain/like/LikeTarget.kt
@@ -3,4 +3,35 @@ package com.jinuk.toy.domain.like
 data class LikeTarget(
     val type: LikeType,
     val id: String,
-)
+) {
+    companion object {
+        fun from(
+            type: LikeType,
+            id: String,
+        ) = LikeTarget(type, id)
+
+        fun from(
+            type: LikeType,
+            id: Long,
+        ) = from(type, id.toString())
+    }
+
+    init {
+        validate(type, id)
+    }
+
+    private fun validate(
+        type: LikeType,
+        id: String,
+    ) {
+        when (type) {
+            LikeType.POST,
+            LikeType.COMMENT,
+            -> require(isNumeric(id)) { "$type ID는 숫자형이어야 합니다." }
+        }
+    }
+
+    private fun isNumeric(id: String): Boolean {
+        return id.toLongOrNull() != null
+    }
+}
