20cfe9daeb7f756c1667d0940c8b404df660fe2c
Jinukix <wfo3977@gmail.com>
2024-10-21 14:12:18 +0900

    redis 모듈에 의존성 추가

--- infra/redis/build.gradle.kts
+++ infra/redis/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.jinuk.toy.lib")
     id("java-test-fixtures")
+    id("com.jinuk.toy.integration-test")
 }
 
 dependencies {
@@ -8,6 +9,9 @@ dependencies {
 
     testFixturesImplementation(Dependencies.SpringBoot.SPRING_BOOT_STARTER_TEST)
     testFixturesImplementation(Dependencies.TestContainers.JUNIT_JUPITER)
+
+    implementation(Dependencies.SpringBoot.SPRING_BOOT_STARTER_REDISSON)
+    implementation(Dependencies.AspectjWeaver.ASPECT_WEAVER)
 
     implementation(project(":util:logger"))
     implementation(project(":util:object-mapper"))
