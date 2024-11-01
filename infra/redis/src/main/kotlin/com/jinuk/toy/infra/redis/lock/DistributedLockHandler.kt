2dad63734afb7796f4ed0bf4d443ed3a65f5d567
ukjjang <ukjjang01@gmail.com>
2024-11-01 18:55:08 +0900

    어노테이션 형태가 아닌 함수형식의 분산락 기능 작성

---
+++ infra/redis/src/main/kotlin/com/jinuk/toy/infra/redis/lock/DistributedLockHandler.kt
@@ -0,0 +1,60 @@
+package com.jinuk.toy.infra.redis.lock
+
+import com.jinuk.toy.infra.redis.lock.DistributedLockHandler.Companion.distributedLockForTransaction
+import com.jinuk.toy.infra.redis.lock.DistributedLockHandler.Companion.redisson
+import org.redisson.api.RedissonClient
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+import java.util.concurrent.TimeUnit
+
+@Component
+class DistributedLockHandler(
+    _redisson: RedissonClient,
+    _distributedLockForTransaction: DistributedLockForTransaction,
+) {
+    init {
+        redisson = _redisson
+        distributedLockForTransaction = _distributedLockForTransaction
+    }
+
+    companion object {
+        lateinit var redisson: RedissonClient
+            private set
+
+        lateinit var distributedLockForTransaction: DistributedLockForTransaction
+            private set
+    }
+}
+
+@Component
+class DistributedLockForTransaction {
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    fun <T> proceed(function: () -> T) = function()
+}
+
+fun <T> distributedLock(
+    key: String,
+    waitTime: Long = 10L,
+    leaseTime: Long = 5L,
+    timeUnit: TimeUnit = TimeUnit.SECONDS,
+    transactional: Boolean = false,
+    function: () -> T,
+): T {
+    val redisLock = redisson.getLock(key)
+    if (!redisLock.tryLock(waitTime, leaseTime, timeUnit)) {
+        throw CannotAcquireLockException("Could not acquire lock. key: $key")
+    }
+
+    return try {
+        if (transactional) {
+            distributedLockForTransaction.proceed(function)
+        } else {
+            function()
+        }
+    } finally {
+        if (redisLock.isLocked && redisLock.isHeldByCurrentThread) {
+            redisLock.unlock()
+        }
+    }
+}
