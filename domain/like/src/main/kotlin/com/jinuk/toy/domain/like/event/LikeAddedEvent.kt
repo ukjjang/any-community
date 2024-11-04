64f9b54e15d2ee67821dc46d67c29f85c2b5e371
Jinukix <wfo3977@gmail.com>
2024-11-04 22:56:22 +0900

    좋아요 발생시, 좋아요 이벤트 발송

---
+++ domain/like/src/main/kotlin/com/jinuk/toy/domain/like/event/LikeAddedEvent.kt
@@ -0,0 +1,16 @@
+package com.jinuk.toy.domain.like.event
+
+import com.jinuk.toy.domain.like.Like
+import com.jinuk.toy.domain.like.LikeTarget
+
+data class LikeAddedEvent(
+    val userId: Long,
+    val likeTarget: LikeTarget,
+) {
+    companion object {
+        fun of(like: Like) =
+            with(like) {
+                LikeAddedEvent(userId, LikeTarget.from(like.targetType, like.id))
+            }
+    }
+}
