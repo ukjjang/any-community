328bffd9cf2d57efda61819ff4fe76fa7c7daf17
ukjjang <ukjjang01@gmail.com>
2024-11-15 00:48:16 +0900

    게시글 작성시 포인트 지급 처리

--- application/src/main/kotlin/com/jinuk/toy/applicaiton/post/command/usecase/CreatePostUsecase.kt
+++ application/src/main/kotlin/com/jinuk/toy/applicaiton/post/command/usecase/CreatePostUsecase.kt
@@ -2,8 +2,12 @@ package com.jinuk.toy.applicaiton.post.command.usecase
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import com.jinuk.toy.applicaiton.point.command.PointCommandBus
+import com.jinuk.toy.applicaiton.point.command.usecase.PointProcessCommand
+import com.jinuk.toy.common.value.point.PointRuleType
 import com.jinuk.toy.common.value.post.PostCategory
 import com.jinuk.toy.common.value.post.PostTitle
+import com.jinuk.toy.domain.point.service.PointRuleQueryService
 import com.jinuk.toy.domain.post.Post
 import com.jinuk.toy.domain.post.service.PostCommandService
 import com.jinuk.toy.domain.post.service.PostQueryService
@@ -12,11 +16,28 @@ import com.jinuk.toy.domain.post.service.PostQueryService
 class CreatePostUsecase(
     private val postQueryService: PostQueryService,
     private val postCommandService: PostCommandService,
+    private val pointRuleQueryService: PointRuleQueryService,
+    private val pointCommandBus: PointCommandBus,
 ) {
+    companion object {
+        private const val POINT_DESCRIPTION_TEMPLATE = "게시글 작성으로 포인트 지급 | 게시글 ID: "
+    }
+
     @Transactional
     operator fun invoke(command: CreatePostCommand): Post {
         require(!postQueryService.existsByTitle(command.title)) { "이미 존재하는 게시글 제목입니다." }
-        return postCommandService.save(command.toPost())
+        return postCommandService.save(command.toPost()).also { pointProcess(it) }
+    }
+
+    private fun pointProcess(post: Post) {
+        val pointRule = pointRuleQueryService.getByRuleType(PointRuleType.POST_CREATION)
+        val processCommand =
+            PointProcessCommand(
+                userId = post.userId,
+                point = pointRule.amount,
+                description = "$POINT_DESCRIPTION_TEMPLATE${post.id}",
+            )
+        pointCommandBus execute processCommand
     }
 }
 
