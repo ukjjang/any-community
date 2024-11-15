a19edcbf803bc7866aab1829a5b8b8501ae9287e
ukjjang <ukjjang01@gmail.com>
2024-11-15 23:44:58 +0900

    포인트 게임 API 개발

---
+++ application/src/main/kotlin/com/jinuk/toy/applicaiton/point/command/usecase/PointGameUsecase.kt
@@ -0,0 +1,41 @@
+package com.jinuk.toy.applicaiton.point.command.usecase
+
+import org.springframework.stereotype.Service
+import com.jinuk.toy.common.value.point.Point
+import com.jinuk.toy.domain.point.pick
+import com.jinuk.toy.domain.point.service.PointGameProbabilityQueryService
+import com.jinuk.toy.domain.user.service.UserQueryService
+import com.jinuk.toy.infra.redis.lock.distributedLock
+
+@Service
+class PointGameUsecase(
+    private val pointGameProbabilityQueryService: PointGameProbabilityQueryService,
+    private val userQueryService: UserQueryService,
+    private val pointProcessUsecase: PointProcessUsecase,
+) {
+    companion object {
+        private val POINT_GAME_COST = Point(-10)
+    }
+
+    operator fun invoke(command: PointGameCommand) = distributedLock(
+        key = "PointProcessUsecase:${command.userId}",
+        transactional = true,
+    ) {
+        val user = userQueryService.getById(command.userId)
+        user.updateTotalPoints(POINT_GAME_COST)
+
+        val point = pointGameProbabilityQueryService.findAll().pick()
+        if (point != Point.ZERO) {
+            val processCommand = PointProcessCommand(
+                userId = command.userId,
+                point = point,
+                description = "description",
+            )
+            pointProcessUsecase(processCommand)
+        }
+    }
+}
+
+data class PointGameCommand(
+    val userId: Long,
+)
