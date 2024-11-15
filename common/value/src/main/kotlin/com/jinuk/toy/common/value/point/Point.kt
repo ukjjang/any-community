8c4eb43f4f5688d583a2203538adb507976bdf28
ukjjang <ukjjang01@gmail.com>
2024-11-15 18:03:22 +0900

    Point 비교를 위해 compareTo

--- common/value/src/main/kotlin/com/jinuk/toy/common/value/point/Point.kt
+++ common/value/src/main/kotlin/com/jinuk/toy/common/value/point/Point.kt
@@ -3,6 +3,7 @@ package com.jinuk.toy.common.value.point
 @JvmInline
 value class Point(
     val value: Long,
-) {
+) : Comparable<Point> {
     operator fun plus(point: Point) = Point(value + point.value)
+    override fun compareTo(other: Point): Int = this.value.compareTo(other.value)
 }
