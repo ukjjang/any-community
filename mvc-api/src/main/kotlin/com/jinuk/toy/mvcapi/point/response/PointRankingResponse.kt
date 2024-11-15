f98aae6c4fca29da985191b413583a9c0e648d16
ukjjang <ukjjang01@gmail.com>
2024-11-15 18:07:00 +0900

    스웨거 주석 추가

--- mvc-api/src/main/kotlin/com/jinuk/toy/mvcapi/point/response/PointRankingResponse.kt
+++ mvc-api/src/main/kotlin/com/jinuk/toy/mvcapi/point/response/PointRankingResponse.kt
@@ -1,13 +1,19 @@
 package com.jinuk.toy.mvcapi.point.response
 
+import io.swagger.v3.oas.annotations.media.Schema
 import com.jinuk.toy.applicaiton.point.query.usecase.GetPointRankingResult
 import com.jinuk.toy.common.value.point.Point
 import com.jinuk.toy.common.value.user.Username
 
+@Schema(description = "포인트 랭킹 유저 조회 응답")
 data class PointRankingResponse(
+    @field:Schema(description = "랭킹", example = "1")
     val ranking: Int,
+    @field:Schema(description = "유저 id", example = "1")
     val userId: Long,
+    @field:Schema(description = "사용자 이름", example = "ukjjang")
     val username: Username,
+    @field:Schema(description = "포인트", example = "6100")
     val totalPoints: Point,
 ) {
     companion object {
