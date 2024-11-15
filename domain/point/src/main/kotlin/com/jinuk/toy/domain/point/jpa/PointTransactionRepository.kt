dce22795b1ad8a9d18cd184d7653c0b386caaec1
ukjjang <ukjjang01@gmail.com>
2024-11-15 22:31:33 +0900

    PointTransactionEntityRepository 클래스명 오타 수정

--- domain/point/src/main/kotlin/com/jinuk/toy/domain/point/jpa/PointTransactionRepository.kt
+++ domain/point/src/main/kotlin/com/jinuk/toy/domain/point/jpa/PointTransactionRepository.kt
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Repository
 import com.jinuk.toy.domain.point.PointTransaction
 import com.jinuk.toy.domain.point.toEntity
 import com.jinuk.toy.domain.point.toModel
-import com.jinuk.toy.infra.rdb.point.jpa.PointEntityTransactionEntityRepository
+import com.jinuk.toy.infra.rdb.point.jpa.PointTransactionEntityRepository
 
 @Repository
 class PointTransactionRepository(
-    private val pointTransactionEntityRepository: PointEntityTransactionEntityRepository,
+    private val pointTransactionEntityRepository: PointTransactionEntityRepository,
 ) {
     fun save(pointTransaction: PointTransaction) =
         pointTransactionEntityRepository.save(pointTransaction.toEntity()).toModel()
--- infra/rdb/src/main/kotlin/com/jinuk/toy/infra/rdb/point/jpa/PointTransactionEntityRepository.kt
+++ infra/rdb/src/main/kotlin/com/jinuk/toy/infra/rdb/point/jpa/PointTransactionEntityRepository.kt
@@ -3,4 +3,4 @@ package com.jinuk.toy.infra.rdb.point.jpa
 import org.springframework.data.jpa.repository.JpaRepository
 import com.jinuk.toy.infra.rdb.point.entity.PointTransactionEntity
 
-interface PointEntityTransactionEntityRepository : JpaRepository<PointTransactionEntity, Long>
+interface PointTransactionEntityRepository : JpaRepository<PointTransactionEntity, Long>
