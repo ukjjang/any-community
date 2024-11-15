5553140567b9bd950f99df976be0381caa479433
ukjjang <ukjjang01@gmail.com>
2024-11-15 22:33:05 +0900

    Point.ZERO 정의

--- common/value/src/main/kotlin/com/jinuk/toy/common/value/point/Point.kt
+++ common/value/src/main/kotlin/com/jinuk/toy/common/value/point/Point.kt
@@ -6,4 +6,8 @@ value class Point(
 ) : Comparable<Point> {
     operator fun plus(point: Point) = Point(value + point.value)
     override fun compareTo(other: Point): Int = this.value.compareTo(other.value)
+
+    companion object {
+        val ZERO = Point(0L)
+    }
 }
