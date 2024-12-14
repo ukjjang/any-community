5566fe6c36869a1bb340f423080cd8754b27dc96
ukjjang <ukjjang01@gmail.com>
2024-12-14 18:46:38 +0900

    outbox retry send scheduler 구현

--- app/consumer/build.gradle.kts
+++ app/consumer/build.gradle.kts
@@ -12,5 +12,6 @@ dependencies {
     implementation(Dependencies.SpringBoot.SPRING_BOOT_STARTER_KAFKA)
 
     implementation(project(":infra:kafka"))
+    implementation(project(":infra:redis"))
     implementation(project(":util:jwt"))
 }
