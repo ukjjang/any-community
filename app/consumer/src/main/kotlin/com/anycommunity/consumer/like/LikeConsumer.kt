d93893978442a33af9545c6109e38d815ab9570c
ukjjang <ukjjang01@gmail.com>
2024-12-26 17:21:56 +0900

    좋아요 추가시 게시글의 좋아요 개수 조정 컨슈머가 아닌 동기로 처리

--- app/consumer/src/main/kotlin/com/anycommunity/consumer/like/LikeConsumer.kt
+++ app/consumer/src/main/kotlin/com/anycommunity/consumer/like/LikeConsumer.kt
@@ -6,27 +6,16 @@ import org.springframework.stereotype.Component
 import com.anycommunity.consumer.global.KafkaEventParser
 import com.anycommunity.definition.global.kafka.KafkaGroupId
 import com.anycommunity.definition.global.kafka.KafkaTopic
-import com.anycommunity.domain.like.event.LikeAddedEvent
 import com.anycommunity.domain.like.event.LikeCanceledEvent
 import com.anycommunity.infra.kafka.KafkaConfig.Companion.LISTENER_FACTORY
 import com.anycommunity.usecase.like.command.LikeCommandBus
-import com.anycommunity.usecase.like.command.usecase.UpdateLikeCountCommand
+import com.anycommunity.usecase.like.command.usecase.internal.UpdateLikeCountCommand
 
 @Component
 class LikeConsumer(
     private val kafkaEventParser: KafkaEventParser,
     private val likeCommandBus: LikeCommandBus,
 ) {
-    @KafkaListener(
-        topics = [KafkaTopic.Like.ADD],
-        groupId = KafkaGroupId.Like.INCREASE_LIKE_COUNT,
-        containerFactory = LISTENER_FACTORY,
-    )
-    fun likeAddedEventConsume(@Payload message: String) {
-        val event = kafkaEventParser.parse(message, LikeAddedEvent::class.java)
-        likeCommandBus execute UpdateLikeCountCommand.from(event)
-    }
-
     @KafkaListener(
         topics = [KafkaTopic.Like.CANCEL],
         groupId = KafkaGroupId.Like.DECREASE_LIKE_COUNT,
