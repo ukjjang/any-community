783391274f0a5312e7b7e7bde233346dbd2644b0
ukjjang <ukjjang01@gmail.com>
2024-12-26 18:30:57 +0900

    게시글 삭제시 게시글 삭제 이벤트 발송

--- definition/src/main/kotlin/com/anycommunity/definition/global/kafka/KafkaTopic.kt
+++ definition/src/main/kotlin/com/anycommunity/definition/global/kafka/KafkaTopic.kt
@@ -2,4 +2,8 @@ package com.anycommunity.definition.global.kafka
 
 object KafkaTopic {
     const val DEAD_LETTER = "dead-letter"
+
+    object Post {
+        const val DELETE = "anycommunity-post-delete"
+    }
 }
