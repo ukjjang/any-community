f472fe6f281f839ce0e277897696cf4b432be74d
ukjjang <ukjjang01@gmail.com>
2024-12-26 18:21:17 +0900

    미사용 이벤트 클래스 제거

--- core/domain/follow/src/main/kotlin/com/anycommunity/domain/follow/event/FollowCanceledEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.anycommunity.domain.follow.event
-
-import com.anycommunity.domain.follow.FollowRelation
-
-data class FollowCanceledEvent(
-    val followRelation: FollowRelation,
-)
--- core/domain/like/src/main/kotlin/com/anycommunity/domain/like/event/LikeCanceledEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.anycommunity.domain.like.event
-
-import com.anycommunity.domain.like.LikeTarget
-
-data class LikeCanceledEvent(
-    val userId: Long,
-    val likeTarget: LikeTarget,
-)
