0d76510fb652d65fb45b706308c2e4d3396c7a02
ukjjang <ukjjang01@gmail.com>
2024-12-26 17:11:52 +0900

    댓글 삭제시 게시글의 댓글 개수 조정 컨슈머가 아닌 동기로 처리

--- core/usecase/src/main/kotlin/com/anycommunity/usecase/comment/command/usecase/DeleteCommentUseCase.kt
+++ core/usecase/src/main/kotlin/com/anycommunity/usecase/comment/command/usecase/DeleteCommentUseCase.kt
@@ -2,29 +2,34 @@ package com.anycommunity.usecase.comment.command.usecase
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import com.anycommunity.definition.global.kafka.KafkaTopic
+import com.anycommunity.definition.global.CountOperation
 import com.anycommunity.definition.like.LikeType
-import com.anycommunity.domain.comment.event.CommentDeletedEvent
 import com.anycommunity.domain.comment.service.CommentCommandService
 import com.anycommunity.domain.like.LikeTarget
 import com.anycommunity.domain.like.service.LikeCommandService
-import com.anycommunity.domain.shared.outbox.OutboxCreator
+import com.anycommunity.usecase.post.command.usecase.internal.UpdatePostCommentCountCommand
+import com.anycommunity.usecase.post.command.usecase.internal.UpdatePostCommentCountUseCase
 
 @Service
 class DeleteCommentUsecase(
     private val commentCommandService: CommentCommandService,
     private val likeCommandService: LikeCommandService,
-    private val outboxCreator: OutboxCreator,
+    private val updatePostCommentCountUseCase: UpdatePostCommentCountUseCase,
 ) {
     @Transactional
     operator fun invoke(command: DeleteCommentCommand) {
         commentCommandService.delete(command.commentId, command.userId, command.postId)
         likeCommandService.delete(LikeTarget.from(LikeType.COMMENT, command.commentId))
 
-        outboxCreator.create(
-            KafkaTopic.Comment.DELETE,
-            CommentDeletedEvent(id = command.commentId, postId = command.postId),
+        updatePostCommentCount(command.postId)
+    }
+
+    private fun updatePostCommentCount(postId: Long) {
+        val command = UpdatePostCommentCountCommand(
+            postId = postId,
+            countOperation = CountOperation.DECREMENT,
         )
+        updatePostCommentCountUseCase(command)
     }
 }
 
