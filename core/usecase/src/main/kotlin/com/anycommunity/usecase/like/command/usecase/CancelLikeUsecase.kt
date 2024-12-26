c1fa2262eb58d4db57e332db8edace16bbf2afdc
ukjjang <ukjjang01@gmail.com>
2024-12-26 17:23:07 +0900

    좋아요 취소시 게시글의 좋아요 개수 조정 컨슈머가 아닌 동기로 처리

--- core/usecase/src/main/kotlin/com/anycommunity/usecase/like/command/usecase/CancelLikeUsecase.kt
+++ core/usecase/src/main/kotlin/com/anycommunity/usecase/like/command/usecase/CancelLikeUsecase.kt
@@ -2,26 +2,29 @@ package com.anycommunity.usecase.like.command.usecase
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import com.anycommunity.definition.global.kafka.KafkaTopic
+import com.anycommunity.definition.global.CountOperation
 import com.anycommunity.domain.like.LikeTarget
-import com.anycommunity.domain.like.event.LikeCanceledEvent
 import com.anycommunity.domain.like.service.LikeCommandService
-import com.anycommunity.domain.shared.outbox.OutboxCreator
+import com.anycommunity.usecase.like.command.usecase.internal.UpdateLikeCountCommand
+import com.anycommunity.usecase.like.command.usecase.internal.UpdateLikeCountUsecase
 
 @Service
 class CancelLikeUsecase(
     private val likeCommandService: LikeCommandService,
-    private val outboxCreator: OutboxCreator,
+    private val updateLikeCountUsecase: UpdateLikeCountUsecase,
 ) {
     @Transactional
     operator fun invoke(command: CancelLikeCommand) {
         likeCommandService.delete(command.userId, command.likeTarget)
+        updateLikeCount(command.likeTarget)
+    }
 
-        val event = LikeCanceledEvent(
-            userId = command.userId,
-            likeTarget = command.likeTarget,
+    private fun updateLikeCount(likeTarget: LikeTarget) {
+        val command = UpdateLikeCountCommand(
+            likeTarget = likeTarget,
+            countOperation = CountOperation.INCREASE,
         )
-        outboxCreator.create(KafkaTopic.Like.CANCEL, event)
+        updateLikeCountUsecase(command)
     }
 }
 
