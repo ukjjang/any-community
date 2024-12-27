92e70702602746991d0f60a8b4b28476ea5e8300
ukjjang <ukjjang01@gmail.com>
2024-12-27 12:06:15 +0900

    user-feed command usecase 네이밍 수정

--- core/usecase/src/main/kotlin/com/anycommunity/usecase/user_feed/command/UserFeedCommandBus.kt
+++ core/usecase/src/main/kotlin/com/anycommunity/usecase/user_feed/command/UserFeedCommandBus.kt
@@ -1,16 +1,16 @@
 package com.anycommunity.usecase.user_feed.command
 
 import org.springframework.stereotype.Service
-import com.anycommunity.usecase.user_feed.command.usecase.UserFeedCreateCommand
+import com.anycommunity.usecase.user_feed.command.usecase.CreateUserFeedCommand
 import com.anycommunity.usecase.user_feed.command.usecase.UserFeedCreateUsecase
-import com.anycommunity.usecase.user_feed.command.usecase.UserFeedDeleteCommandByPostDelete
-import com.anycommunity.usecase.user_feed.command.usecase.UserFeedDeleteCommandByUnFollow
+import com.anycommunity.usecase.user_feed.command.usecase.DeleteUserFeedCommandByPostDelete
+import com.anycommunity.usecase.user_feed.command.usecase.DeleteUserFeedCommandByUnFollow
 import com.anycommunity.usecase.user_feed.command.usecase.UserFeedDeleteUsecase
 
 sealed interface UserFeedCommandBus {
-    infix fun execute(command: UserFeedDeleteCommandByPostDelete)
-    infix fun execute(command: UserFeedDeleteCommandByUnFollow)
-    infix fun execute(command: UserFeedCreateCommand)
+    infix fun execute(command: DeleteUserFeedCommandByPostDelete)
+    infix fun execute(command: DeleteUserFeedCommandByUnFollow)
+    infix fun execute(command: CreateUserFeedCommand)
 }
 
 @Service
@@ -18,7 +18,7 @@ internal class UserFeedCommandBusImpl(
     private val userFeedDeleteUsecase: UserFeedDeleteUsecase,
     private val userFeedCreateUsecase: UserFeedCreateUsecase,
 ) : UserFeedCommandBus {
-    override fun execute(command: UserFeedDeleteCommandByPostDelete) = userFeedDeleteUsecase(command)
-    override fun execute(command: UserFeedDeleteCommandByUnFollow) = userFeedDeleteUsecase(command)
-    override fun execute(command: UserFeedCreateCommand) = userFeedCreateUsecase(command)
+    override fun execute(command: DeleteUserFeedCommandByPostDelete) = userFeedDeleteUsecase(command)
+    override fun execute(command: DeleteUserFeedCommandByUnFollow) = userFeedDeleteUsecase(command)
+    override fun execute(command: CreateUserFeedCommand) = userFeedCreateUsecase(command)
 }
