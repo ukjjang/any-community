c133e6b783789b0e3da74df801e0a211ea044219
ukjjang <ukjjang01@gmail.com>
2024-12-31 12:00:19 +0900

    불필요한 트랜잭션 제거

--- core/domain/shared/src/main/kotlin/com/anycommunity/domain/shared/outbox/OutboxEventListener.kt
+++ core/domain/shared/src/main/kotlin/com/anycommunity/domain/shared/outbox/OutboxEventListener.kt
@@ -2,8 +2,6 @@ package com.anycommunity.domain.shared.outbox
 
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Propagation
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
 import com.anycommunity.domain.shared.config.AsyncConfig.Companion.OUTBOX_POOL
@@ -14,7 +12,6 @@ class OutboxEventListener(
 ) {
 
     @Async(OUTBOX_POOL)
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun send(outbox: Outbox) {
         outboxSender.send(outbox)
