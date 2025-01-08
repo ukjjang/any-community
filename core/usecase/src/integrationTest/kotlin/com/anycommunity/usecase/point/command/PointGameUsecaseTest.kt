33417dd3c011982b7aa61e25e5d7ba27741eeaee
ukjjang <ukjjang01@gmail.com>
2025-01-08 21:01:13 +0900

    fix PointGameUsecase

--- core/usecase/src/integrationTest/kotlin/com/anycommunity/usecase/point/command/PointGameUsecaseTest.kt
+++ core/usecase/src/integrationTest/kotlin/com/anycommunity/usecase/point/command/PointGameUsecaseTest.kt
@@ -1,6 +1,5 @@
 package com.anycommunity.usecase.point.command
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldNotBeBlank
@@ -12,7 +11,6 @@ import com.anycommunity.definition.point.Point
 import com.anycommunity.domain.point.PointGameProbabilityFixture
 import com.anycommunity.domain.point.service.PointGameProbabilityQueryService
 import com.anycommunity.domain.user.UserFixture
-import com.anycommunity.domain.user.service.UserCommandService
 import com.anycommunity.usecase.IntegrationTest
 import com.anycommunity.usecase.point.command.usecase.PointGameCommand
 import com.anycommunity.usecase.point.command.usecase.PointGameUsecase
@@ -20,7 +18,6 @@ import com.anycommunity.usecase.point.command.usecase.internal.PointProcessComma
 import com.anycommunity.usecase.point.command.usecase.internal.PointProcessUsecase
 
 class PointGameUsecaseTest(
-    private val userCommandService: UserCommandService,
     private val userFixture: UserFixture,
 ) : IntegrationTest, DescribeSpec(
     {
@@ -28,7 +25,7 @@ class PointGameUsecaseTest(
             val pointGameProbabilityQueryService: PointGameProbabilityQueryService = mockk(relaxed = true)
             val pointProcessUsecase: PointProcessUsecase = mockk(relaxed = true)
             val pointGameUsecase =
-                PointGameUsecase(pointGameProbabilityQueryService, pointProcessUsecase, userCommandService)
+                PointGameUsecase(pointGameProbabilityQueryService, pointProcessUsecase)
 
             beforeTest {
                 clearMocks(pointGameProbabilityQueryService, pointProcessUsecase)
@@ -43,30 +40,13 @@ class PointGameUsecaseTest(
 
                 pointGameUsecase(PointGameCommand(user.id))
 
-                verify(exactly = 1) {
+                verify(exactly = 2) {
                     pointProcessUsecase(
                         withArg { command ->
                             command.userId shouldBe user.id
-                            command.point shouldBe Point(100)
                             command.description.shouldNotBeBlank()
                         },
                     )
-                }
-            }
-
-            it("포인트가 부족해서 실패") {
-                val user = userFixture.persist(totalPoints = Point(9))
-
-                every { pointGameProbabilityQueryService.findAll() } returns listOf(
-                    PointGameProbabilityFixture.create(point = Point(100), probability = 10000),
-                )
-
-                shouldThrow<IllegalArgumentException> {
-                    pointGameUsecase(PointGameCommand(user.id))
-                }
-
-                verify(exactly = 0) {
-                    pointProcessUsecase(any<PointProcessCommand>())
                 }
             }
 
@@ -74,12 +54,12 @@ class PointGameUsecaseTest(
                 val user = userFixture.persist(totalPoints = Point(10))
 
                 every { pointGameProbabilityQueryService.findAll() } returns listOf(
-                    PointGameProbabilityFixture.create(point = Point(0), probability = 10000),
+                    PointGameProbabilityFixture.create(point = Point.ZERO, probability = 10000),
                 )
 
                 pointGameUsecase(PointGameCommand(user.id))
 
-                verify(exactly = 0) {
+                verify(exactly = 1) {
                     pointProcessUsecase(any<PointProcessCommand>())
                 }
             }
--- core/usecase/src/main/kotlin/com/anycommunity/usecase/point/command/usecase/PointGameUsecase.kt
+++ core/usecase/src/main/kotlin/com/anycommunity/usecase/point/command/usecase/PointGameUsecase.kt
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service
 import com.anycommunity.definition.point.Point
 import com.anycommunity.domain.point.pick
 import com.anycommunity.domain.point.service.PointGameProbabilityQueryService
-import com.anycommunity.domain.user.service.UserCommandService
 import com.anycommunity.infra.redis.lock.distributedLock
 import com.anycommunity.usecase.point.command.usecase.internal.PointProcessCommand
 import com.anycommunity.usecase.point.command.usecase.internal.PointProcessUsecase
@@ -13,10 +12,10 @@ import com.anycommunity.usecase.point.command.usecase.internal.PointProcessUseca
 class PointGameUsecase(
     private val pointGameProbabilityQueryService: PointGameProbabilityQueryService,
     private val pointProcessUsecase: PointProcessUsecase,
-    private val userCommandService: UserCommandService,
 ) {
     companion object {
-        private const val POINT_DESCRIPTION_TEMPLATE = "포인트 게임으로 지급"
+        private const val POINT_GAME_USE_DESCRIPTION_TEMPLATE = "포인트 게임으로 사용"
+        private const val POINT_EARN_DESCRIPTION_TEMPLATE = "포인트 게임으로 지급"
         private val POINT_GAME_COST = Point(-10)
     }
 
@@ -24,16 +23,22 @@ class PointGameUsecase(
         key = "PointProcessUsecase:${command.lockKey}",
         transactional = true,
     ) {
-        userCommandService.updateTotalPoints(command.userId, POINT_GAME_COST)
+        val useCommand = PointProcessCommand(
+            userId = command.userId,
+            point = POINT_GAME_COST,
+            description = POINT_GAME_USE_DESCRIPTION_TEMPLATE,
+        )
+        pointProcessUsecase(useCommand)
 
         val point = pointGameProbabilityQueryService.findAll().pick()
         if (point != Point.ZERO) {
-            val processCommand = PointProcessCommand(
+            val earnCommand = PointProcessCommand(
                 userId = command.userId,
                 point = point,
-                description = POINT_DESCRIPTION_TEMPLATE,
+                description = POINT_EARN_DESCRIPTION_TEMPLATE,
             )
-            pointProcessUsecase(command = processCommand)
+
+            pointProcessUsecase(earnCommand)
         }
     }
 }
