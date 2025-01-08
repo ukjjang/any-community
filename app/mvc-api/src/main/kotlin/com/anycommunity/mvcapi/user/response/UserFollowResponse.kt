4363e378428d30b91e5bb4c23b4edd79b59843f5
ukjjang <ukjjang01@gmail.com>
2025-01-09 00:10:51 +0900

    팔로우, 팔로잉 조회 API 응답의 생성시간 추가

--- app/mvc-api/src/main/kotlin/com/anycommunity/mvcapi/user/response/UserFollowResponse.kt
+++ app/mvc-api/src/main/kotlin/com/anycommunity/mvcapi/user/response/UserFollowResponse.kt
@@ -1,6 +1,7 @@
 package com.anycommunity.mvcapi.user.response
 
 import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDateTime
 import com.anycommunity.definition.user.Username
 import com.anycommunity.usecase.follow.query.usecase.GetFollowerResult
 import com.anycommunity.usecase.follow.query.usecase.GetFollowingResult
@@ -11,16 +12,20 @@ data class UserFollowResponse(
     val id: Long,
     @field:Schema(description = "사용자 이름", example = "ukjjang")
     val username: Username,
+    @field:Schema(description = "팔로우 시간", example = "2024-01-01T12:00:00")
+    val createdAt: LocalDateTime,
 ) {
     companion object {
         fun from(result: GetFollowerResult) = UserFollowResponse(
             id = result.id,
             username = result.username,
+            createdAt = result.createdAt,
         )
 
         fun from(result: GetFollowingResult) = UserFollowResponse(
             id = result.id,
             username = result.username,
+            createdAt = result.createdAt,
         )
     }
 }
