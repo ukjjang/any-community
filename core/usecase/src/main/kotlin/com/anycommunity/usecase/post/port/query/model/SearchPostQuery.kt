cabdeb8487391fe020b094a6b36334839941e69d
ukjjang <ukjjang01@gmail.com>
2025-01-09 15:34:21 +0900

    게시글 카테고리 검색 기능 제공

--- core/usecase/src/main/kotlin/com/anycommunity/usecase/post/port/query/model/SearchPostQuery.kt
+++ core/usecase/src/main/kotlin/com/anycommunity/usecase/post/port/query/model/SearchPostQuery.kt
@@ -2,12 +2,14 @@ package com.anycommunity.usecase.post.port.query.model
 
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
+import com.anycommunity.definition.post.PostSearchCategory
 import com.anycommunity.definition.post.PostSearchSortType
 
 data class SearchPostQuery(
     val keyword: String?,
     val page: Int,
     val size: Int,
+    val postSearchCategory: PostSearchCategory,
     val postSearchSortType: PostSearchSortType,
 ) {
     fun pageable(): Pageable = PageRequest.of(page - 1, size)
