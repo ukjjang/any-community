f9c65cc51908a907502ebb6f115c6f5809af57d5
ukjjang <ukjjang01@gmail.com>
2025-01-12 11:00:10 +0900

    메시지 파싱 성공 로그 추가

--- app/consumer/src/main/kotlin/com/anycommunity/consumer/global/KafkaEventParser.kt
+++ app/consumer/src/main/kotlin/com/anycommunity/consumer/global/KafkaEventParser.kt
@@ -4,16 +4,22 @@ import com.fasterxml.jackson.databind.JavaType
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Component
 import com.anycommunity.infra.kafka.model.KafkaMessage
+import com.anycommunity.util.logger.LazyLogger
 
 @Component
 class KafkaEventParser(private val objectMapper: ObjectMapper) {
+    companion object {
+        private val log by LazyLogger()
+    }
+
     fun <T> parse(message: String, payloadClass: Class<T>): T =
         parse<T>(message, objectMapper.constructType(payloadClass)).payload
 
     private fun <T> parse(message: String, payloadType: JavaType): KafkaMessage<T> {
         require(message.isNotBlank()) { "Message is blank" }
         try {
-            return objectMapper.readValue(message, convertToMessageType(payloadType))
+            return objectMapper.readValue<KafkaMessage<T>?>(message, convertToMessageType(payloadType))
+                .also { log.info { "Successfully parsed message to event: $it" } }
         } catch (e: Exception) {
             throw IllegalArgumentException("could not parse the message: $message", e)
         }
